1c628ac7cd36de046d541513f33798b0028e1178
HafizullahIT19 <tisudd19hafrah@gmail.com>
2021-04-26 13:14:01 +0200

    video-container update

--- js/main.js
+++ js/main.js
@@ -1,26 +1,71 @@
 
-const iconGoesUp = document.querySelector("#iconGoesUp");
-
+// ----------------- Scroll Event Start -------------------
+const iconGoesUp = document.querySelector('#iconGoesUp');
 
 //onscroll Event
 window.addEventListener('scroll', function () {
     const scrollY = window.scrollY;
-    
+
     if (scrollY > 332) {
-        iconGoesUp.style.display = "inline";
-        console.log('storeeeee');
+        iconGoesUp.style.display = 'inline';
+        // console.log('storeeeee');
     } else {
-        console.log('mindreeeee');
-        iconGoesUp.style.display = "none";
+        iconGoesUp.style.display = 'none';
+        // console.log('mindreeeee');
     }
-    console.log(scrollY);
+    // console.log(scrollY);
 });
 
+// Scroll Event End
 
 
-//example
-const btn = document.querySelector('#play');
+// ----------------- Video Start -------------------
 
-btn.addEventListener('click', function () {
-    console.log('Helloooooo');
+const video = document.querySelector('#video');
+const videoContent = document.querySelector('#video-content');
+const playBtn = document.querySelector('#play');
+const volumeBtn = document.querySelector('#volume');
+
+//Play Btn
+playBtn.addEventListener('click', function () {
+    if (video.paused) {
+        video.play();
+        playBtn.classList.remove("fas", "fa-pause-circle")
+        playBtn.classList.add("fas", "fa-play-circle")
+        videoContent.style.display = "none"
+        console.log('Play');
+
+    } else {
+        video.pause();
+        playBtn.classList.remove("fas", "fa-play-circle")
+        playBtn.classList.add("fas", "fa-pause-circle")
+        videoContent.style.display = "inline"
+        console.log('Pause');
+    }
 });
+
+//Volume Btn
+volumeBtn.addEventListener('click', function () {
+    if (video.paused) {
+        video.play();
+        volumeBtn.classList.remove("fas", "fa-volume-mute")
+        volumeBtn.classList.add("fas", "fa-volume-down")
+        console.log('Volume Down');
+
+    } else {
+        video.pause();
+        volumeBtn.classList.remove("fas", "fa-volume-down")
+        volumeBtn.classList.add("fas", "fa-volume-mute")
+        console.log('Volume Mute');
+    }
+});
+
+//Video End
+
+
+
+// ----------------- Login Btn Start -------------------
+
+// Login Btn End
+
+
