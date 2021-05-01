4e2935e0fa3b1513486e3fd80c79a6ffba54c6dc
HafizullahRahimi <Hafizullahrahimi@outlook.com>
2021-05-01 12:17:02 +0200

    header and header-search  create

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 
+console.log("Hello");
+
+function showMenu() {
+    console.log('Menu');
+    const menu = document.getElementById('menu');
+    if (menu.className === 'header-menu') {
+        menu.className += ' show';
+    } else {
+        menu.className = 'header-menu';
+    }
+}
+
 // ----------------- Scroll Event Start -------------------
 const iconGoesUp = document.querySelector('#iconGoesUp');
 
@@ -18,7 +30,6 @@ window.addEventListener('scroll', function () {
 
 // Scroll Event End
 
-
 // ----------------- Video Start -------------------
 
 const video = document.querySelector('#video');
@@ -27,45 +38,39 @@ const playBtn = document.querySelector('#play');
 const volumeBtn = document.querySelector('#volume');
 
 //Play Btn
-playBtn.addEventListener('click', function () {
-    if (video.paused) {
-        video.play();
-        playBtn.classList.remove("fas", "fa-pause-circle")
-        playBtn.classList.add("fas", "fa-play-circle")
-        videoContent.style.display = "none"
-        console.log('Play');
-
-    } else {
-        video.pause();
-        playBtn.classList.remove("fas", "fa-play-circle")
-        playBtn.classList.add("fas", "fa-pause-circle")
-        videoContent.style.display = "inline"
-        console.log('Pause');
-    }
-});
+// playBtn.addEventListener('click', function () {
+//     if (video.paused) {
+//         video.play();
+//         playBtn.classList.remove('fas', 'fa-pause-circle');
+//         playBtn.classList.add('fas', 'fa-play-circle');
+//         videoContent.style.display = 'none';
+//         console.log('Play');
+//     } else {
+//         video.pause();
+//         playBtn.classList.remove('fas', 'fa-play-circle');
+//         playBtn.classList.add('fas', 'fa-pause-circle');
+//         videoContent.style.display = 'inline';
+//         console.log('Pause');
+//     }
+// });
 
 //Volume Btn
-volumeBtn.addEventListener('click', function () {
-    if (video.paused) {
-        video.play();
-        volumeBtn.classList.remove("fas", "fa-volume-mute")
-        volumeBtn.classList.add("fas", "fa-volume-down")
-        console.log('Volume Down');
-
-    } else {
-        video.pause();
-        volumeBtn.classList.remove("fas", "fa-volume-down")
-        volumeBtn.classList.add("fas", "fa-volume-mute")
-        console.log('Volume Mute');
-    }
-});
+// volumeBtn.addEventListener('click', function () {
+//     if (video.paused) {
+//         video.play();
+//         volumeBtn.classList.remove('fas', 'fa-volume-mute');
+//         volumeBtn.classList.add('fas', 'fa-volume-down');
+//         console.log('Volume Down');
+//     } else {
+//         video.pause();
+//         volumeBtn.classList.remove('fas', 'fa-volume-down');
+//         volumeBtn.classList.add('fas', 'fa-volume-mute');
+//         console.log('Volume Mute');
+//     }
+// });
 
 //Video End
-
-
 
 // ----------------- Login Btn Start -------------------
 
 // Login Btn End
-
-
